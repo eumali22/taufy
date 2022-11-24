46a2a75eff0d00ace387d9d68a263c5aeee89ad6
Erick Kristofer Umali <eumali2022@gmail.com>
2022-11-24 23:24:03 +0800

    Create YNAB transaction

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,51 @@
-console.log("Hello world!");
+import * as dotenv from 'dotenv';
+import { getAllBudgets, postTransaction } from './ynab.js';
+
+dotenv.config();
+
+console.log('Hello world!');
+
+const ynabToken = process.env.YNAB_TOKEN || '';
+
+// (async function() {
+//   const budgets = await getAllBudgets(ynabToken);
+//   for (let b of budgets) {
+//     console.log(b);
+//   }
+// })();
+
+/*
+(async function() {
+  const eToroPayload = {
+    "transaction": {
+      "account_id": process.env.ETORO_ACCT_ID,
+      "date": "2022-11-24",
+      "amount": 10000,
+      // "payee_id": "string",
+      // "payee_name": "string",
+      // "category_id": "string",
+      // "memo": "string",
+      "cleared": "cleared",
+      "approved": true,
+      // "flag_color": "red",
+      // "import_id": "string",
+      // "subtransactions": [
+      //   {
+      //     "amount": 0,
+      //     "payee_id": "string",
+      //     "payee_name": "string",
+      //     "category_id": "string",
+      //     "memo": "string"
+      //   }
+      // ]
+    },
+  }
+
+  const response = await postTransaction(ynabToken, process.env.BUDGET_ID || '', eToroPayload);
+  if (response.status !== 201) {
+    console.log(`Create transaction failed: ${response.status} ${response.statusText}`);
+  } else {
+    console.log(`Create transaction success: ${response.status} ${response.statusText}`);
+  }
+})();
+*/
---
+++ src/ynab.ts
@@ -0,0 +1,41 @@
+import fetch from 'node-fetch';
+
+type BudgetJson = {
+  data: {
+    budgets: Array<object>,
+    default_budget: unknown
+  }
+};
+
+type AssertBudgetJsonFn = (value: unknown) => asserts value is BudgetJson;
+
+const assertBudgetJson: AssertBudgetJsonFn = (value) => {
+  if (!value || typeof value !== 'object' || (typeof (value as BudgetJson).data !== 'object')) {
+    throw new Error('error in parsing fetched value to BudgetJson type');
+  }
+}
+
+export const getAllBudgets = async (token: string) => {
+  const response = await fetch('https://api.youneedabudget.com/v1/budgets', {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    },
+  });
+
+  const json = await response.json();
+  assertBudgetJson(json);
+  return json.data.budgets;
+}
+
+export const postTransaction = async (token: string, budgetId: string, payload: object) => {
+  const response = await fetch(`https://api.youneedabudget.com/v1//budgets/${budgetId}/transactions`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    },
+    body: JSON.stringify(payload)
+  });
+  return response;
+}
